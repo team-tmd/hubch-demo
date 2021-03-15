734fb26a20039d2050fea7c521f2f51ab97a94ee
tomotakashimizu <tomotaka.developer@gmail.com>
2021-03-15 18:38:58 +0900

    MyPage 画面の作成

---
+++ src/components/MyProfile.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="my-profile">
+    <h1>This is my page</h1>
+    <button v-on:click="signOut">ログアウトする</button>
+    <h2>ログインしているユーザー情報</h2>
+    <img :src="user.photoURL" class="rounded" />
+    <div>Googleアカウント：{{ user.email }}</div>
+    <div>ユーザー名：{{ user.displayName }}</div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase"
+
+export default {
+  methods: {
+    signOut() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          // Sign-out successful.
+        })
+        .catch((error) => {
+          // An error happened.
+          console.log(error)
+        })
+    },
+  },
+  computed: {
+    user() {
+      return firebase.auth().currentUser
+    },
+  },
+}
+</script>
+
+<style scoped>
+.rounded {
+  border-radius: 50px;
+}
+</style>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="sign-in">
+    <h1>This is sign-in page</h1>
+    <button v-on:click="signIn">ログインする</button>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase"
+
+export default {
+  methods: {
+    signIn() {
+      var provider = new firebase.auth.GoogleAuthProvider()
+      // firebase.auth().signInWithRedirect(provider)
+
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(() => {
+          /** @type {firebase.auth.OAuthCredential} */
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          console.log(error)
+        })
+    },
+  },
+}
+</script>
